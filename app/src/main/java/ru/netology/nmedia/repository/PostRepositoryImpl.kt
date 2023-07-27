21bf9a2f914433071328d8dd662a8e52858c0d99
alexthegreatest48 <115409767+alexthegreatest48@users.noreply.github.com>
2023-07-27 20:23:14 +0300

    Glide added

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -2,11 +2,15 @@ package ru.netology.nmedia.repository
 
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
 import ru.netology.nmedia.dto.Post
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 
@@ -22,22 +26,35 @@ class PostRepositoryImpl: PostRepository {
         private val jsonType = "application/json".toMediaType()
     }
 
-    override fun getAll(): List<Post> {
+    override fun getAll(callback: PostRepository.PostCallback<List<Post>>) {
         val request: Request = Request.Builder()
             .url("${BASE_URL}/api/slow/posts")
             .build()
 
         return client.newCall(request)
-            .execute()
-            .let { it.body?.string() ?: throw RuntimeException("body is null") }
-            .let {
-                gson.fromJson(it, typeToken.type)
-            }
+            .enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    callback.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if(!response.isSuccessful) {
+                        callback.onError(Exception(response.message))
+                    }else{
+                        callback.onSuccess(
+                            gson.fromJson(
+                                requireNotNull(response.body?.string()),
+                                typeToken.type
+                            )
+                        )
+                    }
+                }
+        })
     }
 
 
 
-    override fun likeById(post: Post): Post {
+    override fun likeById(post: Post, callback: PostRepository.PostCallback<Post>) {
         val id = post.id
         val request: Request = Request.Builder()
             .post(gson.toJson(post).toRequestBody(jsonType))
@@ -45,26 +62,52 @@ class PostRepositoryImpl: PostRepository {
             .build()
 
         return client.newCall(request)
-            .execute()
-            .let { it.body?.string() ?: throw RuntimeException("body is null") }
-            .let {
-                gson.fromJson(it, Post::class.java)
-            }
+            .enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    callback.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if(!response.isSuccessful) {
+                        callback.onError(Exception(response.message))
+                    }else{
+                        callback.onSuccess(
+                            gson.fromJson(
+                                requireNotNull(response.body?.string()),
+                                Post::class.java
+                            )
+                        )
+                    }
+                }
+            })
     }
 
-    override fun unlikeById(post: Post): Post {
+    override fun unlikeById(post: Post, callback: PostRepository.PostCallback<Post>){
         val id = post.id
         val request: Request = Request.Builder()
-            .delete()
+            .delete(gson.toJson(post).toRequestBody(jsonType))
             .url("${BASE_URL}/api/slow/posts/$id/likes")
             .build()
 
         return client.newCall(request)
-            .execute()
-            .let { it.body?.string() ?: throw RuntimeException("body is null") }
-            .let {
-                gson.fromJson(it, Post::class.java)
-            }
+            .enqueue(object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    callback.onError(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if(!response.isSuccessful) {
+                        callback.onError(Exception(response.message))
+                    }else{
+                        callback.onSuccess(
+                            gson.fromJson(
+                                requireNotNull(response.body?.string()),
+                                Post::class.java
+                            )
+                        )
+                    }
+                }
+            })
     }
 
     override fun save(post: Post) {
